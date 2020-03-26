f2c6ff0ff27ccb62108e0c244859d6130e80745b
Maude Lizaire <maude_liz@hotmail.ca>
2020-03-26 16:12:14 -0400

    Add main()

--- HW2/assignment2/gradient.py
+++ HW2/assignment2/gradient.py
@@ -4,10 +4,11 @@ import numpy as np
 import collections
 import torch
 import torch.nn as nn
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 from solution_copy import RNN, GRU
 from torch.autograd import Variable
-from helper_functions import ptb_iterator, ptb_raw_data, repackage_hidden
+from helper_functions import ptb_iterator, ptb_raw_data, repackage_hidden, init_device
 
 
 #------------------- Define device
@@ -21,7 +22,8 @@ else:
 
 
 #------------------ LOAD DATA
-data_path  = "/Users/mlizaire/Codes/IFT6135/HW2/assignment2/data/"
+# data_path  = "/Users/mlizaire/Codes/IFT6135/HW2/assignment2/data/"
+data_path  = "data"
 raw_data = ptb_raw_data(data_path)
 train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
 
@@ -30,54 +32,33 @@ train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
 
 # #---------------PARAMETERS
 # MODEL_PATH = "/Users/mlizaire/Codes/IFT6135/HW2/assignment2/results/results/job_6253799_3p2/results/GRU_ADAM_model=GRU_optimizer=ADAM_initial_lr=0.001_batch_size=128_seq_len=35_hidden_size=512_num_layers=2_dp_keep_prob=0.5_num_epochs=40_save_best_save_dir=results_0"
-# # EMB_SIZE = 200
-# # HIDDEN_SIZE = 512
-# BATCH_SIZE = 128
-# # VOCAB_SIZE = 10000
-# # NUM_LAYERS = 2
-# # DP_KEEP_PROB = 0.5
-# # SEQ_LEN = 35
-# GENERATED_SEQ_LEN = 34
+MODEL_PATH = "results/results/job_6253799_3p2/results/GRU_ADAM_model=GRU_optimizer=ADAM_initial_lr=0.001_batch_size=128_seq_len=35_hidden_size=512_num_layers=2_dp_keep_prob=0.5_num_epochs=40_save_best_save_dir=results_0"
 
-# #--------------- LOAD MODEL
-# load_path = os.path.join(MODEL_PATH, 'best_params.pt')
-# model = GRU(emb_size=200,
-#                     hidden_size=512,
-#                     seq_len=35,
-#                     batch_size=128,
-#                     vocab_size=10000,
-#                     num_layers=2,
-#                     dp_keep_prob=0.5, 
-#                     keep_hidden_states=True)
+# EMB_SIZE = 200
+# HIDDEN_SIZE = 512
+BATCH_SIZE = 128
+# VOCAB_SIZE = 10000
+# NUM_LAYERS = 2
+# DP_KEEP_PROB = 0.5
+# SEQ_LEN = 35
+GENERATED_SEQ_LEN = 34
 
-# model.load_state_dict(torch.load(load_path, map_location='cpu'))
+#--------------- LOAD MODEL
+load_path = os.path.join(MODEL_PATH, 'best_params.pt')
+model = GRU(emb_size=200,
+                    hidden_size=512,
+                    seq_len=35,
+                    batch_size=128,
+                    vocab_size=10000,
+                    num_layers=2,
+                    dp_keep_prob=0.5, 
+                    keep_hidden_states=True)
+
+model.load_state_dict(torch.load(load_path, map_location='cpu'))
 
 # hidden = model.init_hidden()
 # hidden = hidden.to(device)
 
-
-# #---------------MODEL PARAMETERS
-MODEL_PATH = '/Users/mlizaire/Codes/IFT6135/HW2/assignment2/results/results/job_6248705_3p1/results/RNN_SGD_model=RNN_optimizer=SGD_initial_lr=1.0_batch_size=128_seq_len=35_hidden_size=512_num_layers=2_dp_keep_prob=0.8_num_epochs=40_save_best_save_dir=results_0'
-EMB_SIZE = 200
-HIDDEN_SIZE = 512
-BATCH_SIZE = 128
-VOCAB_SIZE = 10000
-NUM_LAYERS = 2
-DP_KEEP_PROB = 0.8
-SEQ_LEN = 35
-GENERATED_SEQ_LEN = 34
-#----------------- LOAD MODEL-------------------------------- 
-load_path = os.path.join(MODEL_PATH, 'best_params.pt')
-model = RNN(emb_size=EMB_SIZE,
-                    hidden_size=HIDDEN_SIZE,
-                    seq_len=SEQ_LEN,
-                    batch_size=BATCH_SIZE,
-                    vocab_size=VOCAB_SIZE,
-                    num_layers=NUM_LAYERS,
-                    dp_keep_prob=DP_KEEP_PROB, 
-                     keep_hidden_states=True)
-
-model.load_state_dict(torch.load(load_path, map_location='cpu'))
 
 #-----------Loss function ------------------------------
 loss_fn = nn.CrossEntropyLoss()
@@ -128,66 +109,133 @@ def compute_gradients(model, data, loss_fn ):
             concat_avg_list_hidden_states = torch.cat((batch_avg_list_hidden_states[0],batch_avg_list_hidden_states[1]), dim=1)
             print("check concat", concat_avg_list_hidden_states.shape)
             print("check norm", torch.norm(concat_avg_list_hidden_states, dim = 1).shape)
-
-            # plt.plot(np.arange(35),torch.norm(concat_avg_list_hidden_states, dim = 1) )
-            # plt.show()
-            # for layer in np.arange(model.num_layers):
-            #     print("layer=", layer)
-            #     hidden_t = torch.empty([model.num_layers,model.batch_size, model.hidden_size ])
-            #     grads.append([])
-            #     norm_grads.append([])
-            #     for t in np.arange(model.seq_len):
-            #         # print("timestep=", t)
-            #         hidden_t[layer] = model.list_hidden_states[layer][t]
-            #         # print("hidden_t", model.list_hidden_states[layer][t].grad)
-            #         grad_hidden_t = hidden_t[layer].grad
-            #         # print("grad_hidden_t", grad_hidden_t)
-            #         grads[layer].append(hidden_t[layer])
-            #         norm_grads[layer].append(hidden_t[layer].norm())
-            #         # print('norm_grads', norm_grads)
-            # # print("norm", norm_grads[0][3])
-            # # print("hiden",hidden_t[0] )
-            # # for t in np.arange(model.seq_len):
-            # #     hidden_t = model.list_hidden_states[0][t]
-            # #     grad = torch.empty(0)
-            # #     for layer in np.range(model.num_layers):
-            # #         grad[layer] = hidden_t[layer].grad()
-            # #         grads[layer].append(grad[layer])
-            # #         norm_grads[layer].append(grad[layer].norm)
         
             return torch.norm(concat_avg_list_hidden_states, dim = 1)
 
 
 ##----------------------
-norm_gradients = compute_gradients(model, train_data, loss_fn=loss_fn )
-# print("Norm",len(norm_gradients[0]))
-t = np.arange(model.seq_len)
+# norm_gradients = compute_gradients(model, train_data, loss_fn=loss_fn )
+# t = np.arange(model.seq_len)
 
-# for t in np.arange(model.seq_len):
-#     print("t=",t)
-#     print("norme", norm_gradients[t])
 
-plt.plot(t, norm_gradients.tolist(), "-o")
-plt.show()
+
+# plt.plot(t, norm_gradients.tolist(), "-o")
+# print("Here is you figure!")
+# plt.show()
           
 
-    # for layer in np.range(model.num_layers):
-    #     batch_grads = torch.autograd.grad(loss_final_timestep, layer_states, retain_graph=True)
-    #     avg_grads = [torch.mean(bg, 0) for bg in batch_grads]
-# plt.plot(t, norm_gradients[0])
-# plt.show()
 
+
+def plot_loss_vs_timestep(models_dic, grad_dic):
+    mpl.rcParams['font.size'] = 24.
+    mpl.rcParams['font.family'] = 'Arial'
+
+    t = np.arange(35)
+
+    fig , axes = plt.subplots(1,1,figsize=(7,7)) 
+    fig.subplots_adjust(left=0.18, right=.9, bottom=0.15, top=0.9);
+    axes.set_ylim([0, 0.0016])
+    axes.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axes.set_xlim([0, 35])
+
+    axes.set_xlabel(r"Time-step ($t$)")
+    axes.set_ylabel(r"||$\nabla_t \mathcal{L}_T$||")
+
+    for archi in models_dic:
+        axes.plot(t, grad_dic[archi].tolist(), "-o", color = models_dic[archi][2], label = str(archi) )
+    
+    axes.legend()
+    plt.show()
+    fig.savefig("./Figures/gradientsLoss.pdf")
+    plt.close()
+
+
+
+def main():
+    print("Executing ||Main|| to compute gradients")
+    
+    device = init_device()
+    print('||Main||  Device :', device)
+    
+    print('||Main||  Loading Data')
+    train_data, valid_data, test_data, word_to_id, id_2_word = ptb_raw_data(data_path="data")
+    vocab_size = len(word_to_id)
+
+    models_dic = {'RNN' : [RNN(emb_size = 200,
+                              hidden_size = 512,
+                              seq_len = 35,
+                              batch_size = 128,
+                              vocab_size = vocab_size,
+                              num_layers = 2,
+                              dp_keep_prob = 0.8, 
+                              keep_hidden_states=True),
+                              "results/results/job_6248705_3p1/results/RNN_SGD_model=RNN_optimizer=SGD_initial_lr=1.0_batch_size=128_seq_len=35_hidden_size=512_num_layers=2_dp_keep_prob=0.8_num_epochs=40_save_best_save_dir=results_0",
+                              "crimson"
+                            ],
+
+                  'GRU' : [GRU(emb_size = 200,
+                              hidden_size = 512,
+                              seq_len = 35,
+                              batch_size = 128,
+                              vocab_size = vocab_size,
+                              num_layers = 2,
+                              dp_keep_prob = 0.5, 
+                              keep_hidden_states = True), 
+                              "results/results/job_6253799_3p2/results/GRU_ADAM_model=GRU_optimizer=ADAM_initial_lr=0.001_batch_size=128_seq_len=35_hidden_size=512_num_layers=2_dp_keep_prob=0.5_num_epochs=40_save_best_save_dir=results_0",
+                              "royalblue"
+                            ]
+                 }
+    
+    grad_dic = {'RNN' : None, 'GRU' : None}
+    
+    for archi in models_dic:
+        print('||Main||  Loading ', str(archi))
+        model = models_dic[archi][0]
+        load_path = os.path.join(models_dic[archi][1], 'best_params.pt')
+        model.load_state_dict(torch.load(load_path, map_location='cpu'))
+
+        loss_fn = nn.CrossEntropyLoss()
+
+        print('||Main||  Computing gradients ...')
+        grad_dic[archi] = compute_gradients(model, train_data, loss_fn=loss_fn )
+
+    print('||Main||  Ploting figure')
+    plot_loss_vs_timestep(models_dic, grad_dic)
+
+    print('||Main||  DONE ;)')
+
+
+if __name__== '__main__':
+    main()
 #%%
-# x = torch.randn(2, 2, requires_grad=True)
-# y = x.clone()
-# y.retain_grad()
+# models_dic = {'RNN' : [RNN(emb_size = 200,
+#                         hidden_size = 512,
+#                         seq_len = 35,
+#                         batch_size = 128,
+#                         vocab_size = 10000,
+#                         num_layers = 2,
+#                         dp_keep_prob = 0.8, 
+#                         keep_hidden_states=True),
+#                         "results/results/job_6248705_3p1/results/RNN_SGD_model=RNN_optimizer=SGD_initial_lr=1.0_batch_size=128_seq_len=35_hidden_size=512_num_layers=2_dp_keep_prob=0.8_num_epochs=40_save_best_save_dir=results_0"
+#                     ],
 
-# z = y**2
-# z.mean().backward()
+#             'GRU' : [GRU(emb_size = 200,
+#                         hidden_size = 512,
+#                         seq_len = 35,
+#                         batch_size = 128,
+#                         vocab_size = 10000,
+#                         num_layers = 2,
+#                         dp_keep_prob = 0.5, 
+#                         keep_hidden_states = True),
+#                         "results/results/job_6253799_3p2/results/GRU_ADAM_model=GRU_optimizer=ADAM_initial_lr=0.001_batch_size=128_seq_len=35_hidden_size=512_num_layers=2_dp_keep_prob=0.5_num_epochs=40_save_best_save_dir=results_0" 
+#                     ]
+#             }
 
-# print(y.grad)
-# print(x.grad)
+# for archi in models_dic:
+#     print(models_dic[archi][1])
+#     print(models_dic["RNN"][1])
+        
 
 
 
-# %%
+
